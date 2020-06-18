cecf24761b87336aedcf9df09825c2c502e68a0e
artem54325 <chilo997@mail.ru>
2020-06-18 19:00:02 +0500

    create application

--- src/index.js
+++ src/index.js
@@ -2,11 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import Header from './views/layouts/_layoutHeader';
+import Articles from './views/articles/index';
+import CreateTests from './views/createTests/index';
+import createThemes from './views/createThemes/index';
+import Menu from './views/menu/index';
+import Meeting from './views/meeting/index';
+import TestingTests from './views/testingTests/index';
+import Users from './views/users/index';
+import VideoCall from './views/articles/index';
+
+
+const NoMatchPage = () => {
+  return (
+    <h3>404 - Not found</h3>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <Header/>
+        <React.Fragment>
+            <Switch>
+                <Route path="/" component={Menu} exact={true}/>
+                <Route path="/Articles" component={Articles}/>
+                <Route path="/Tests" component={CreateTests}/>
+                <Route path="/Themes" component={createThemes}/>
+                <Route path="/Meeting" component={Meeting}/>
+                <Route path="/Testing" component={TestingTests}/>
+                <Route path="/Settings" component={Users}/>
+                <Route path="/VideoCalls" component={VideoCall}/>
+                {/* <Redirect to="/" /> */}
+                <Route component={NoMatchPage} />
+            </Switch>
+        </React.Fragment>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
