c9942b2c9d29a6c24d25dc743dd6ba584f467fbb
artem54325 <chilo997@mail.ru>
2020-06-27 08:41:27 +0000

    updated constants

--- src/helpers/Contants.js
+++ src/helpers/Contants.js
@@ -1,3 +1,3 @@
 // export const URL = "http://192.168.1.202:5000/api/"
 // export const URL = "https://localhost:5001/api/"
-export const URL = "http://localhost:5003/api/"
+export const URL = "http://192.168.1.202:5003/api/"
